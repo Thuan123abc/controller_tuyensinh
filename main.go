d7552d247638b9b19be93c9471d09f2fe5689261
Thuan123abc <110764062+Thuan123abc@users.noreply.github.com>
2022-08-14 15:54:50 +0700

    update main

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package controller_tuyensinh
+import (
+	"controller_tuyensinh/controller"
+	"controller_tuyensinh/database"
+	"controller_tuyensinh/view"
+)
+
+func main() {
+	db := database.NewDB()
+	eRepo := database.NewStudentRepo(db)
+	eController := controller.NewStudentController(eRepo)
+	eView := view.NewStudentView(eController)
+
+	eView.CreateStudent()
+}
